084628961da6bdb06f447d0d2524e2e1b0ee01ea
Oscar <oscar.martinez.b@cua.uam.mx>
2025-04-07 16:49:20 -0600

    connection

--- src/main/java/mx/uam/proyecto/blDto/PiezasDto.java
+++ src/main/java/mx/uam/proyecto/blDto/PiezasDto.java
@@ -1,5 +1,43 @@
 package mx.uam.proyecto.blDto;
 
 public class PiezasDto {
-    
+    private String nombre;
+    private int id;
+    private String descripcion;
+
+    public PiezasDto(String nombre, int id, String descripcion) {
+        this.nombre = nombre;
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+    public PiezasDto() {
+    }
+    public PiezasDto(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
