0f3b89ddbbf0b27764c13eaf374a0eb7c8f3720e
oswaldo <oswaldomejia791@gmail.com>
2025-04-07 16:42:27 -0600

    gestor

--- src/main/java/mx/uam/proyecto/bl/GestorPiezas.java
+++ src/main/java/mx/uam/proyecto/bl/GestorPiezas.java
@@ -1,5 +1,30 @@
 package mx.uam.proyecto.bl;
 
-public class GestorPiezas {
+import mx.uam.proyecto.blDto.PiezasDto;
+import mx.uam.proyecto.dal.PiezasDao;
+import mx.uam.proyecto.dalEntities.Piezas;
+import mx.uam.proyecto.dl.PiezaDao;
 
+public class GestorPiezas {
+    PiezasDao piezaDao = new PiezaDao();
+
+    public PiezasDto agregarPieza(Piezas piezas) {
+        return null;
+    }
+
+    public PiezasDto modificarPieza(Piezas piezas) {
+        return null;
+    }
+
+    public boolean eliminarPieza(int id) {
+        return false;
+    }
+
+    public PiezasDto buscarPieza(int id) {
+        return null;
+    }
+
+    public PiezasDto getAllPiezas() {
+        return null;
+    }
 }
