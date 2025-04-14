0bc35a136a0b9301e7cb65843449f4d01645d838
Aaron <aaronrodrigo@hotmail.com>
2025-04-14 14:47:12 -0600

    seborraclaseDAO

--- src/main/java/mx/uam/proyecto/dal/Dao.java
+++
@@ -1,8 +0,0 @@
-package mx.uam.proyecto.dal;
-
-public interface  Dao<T> {
-    public void getAll();
-    public int save(T entity);
-    public void  update (T entity);
-    public boolean delete (int id);
-}
